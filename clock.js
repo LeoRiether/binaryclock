a7f394481bd8f2c9827a825ddc717d8ddcb6457c
Leonardo Riether <leonardoalves8217@gmail.com>
2016-02-20 17:58:19 -0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,20 @@
-
+(function(win, doc){
+  'use strict';
+  
+  var $ = doc.querySelector.bind(doc)
+  
+  setInterval(function () {
+    var 
+      d = new Date(),
+      bh = d.getHours().toString(2)
+      bm = d.getHours().toString(2)
+    ;
+    
+    for(var i = 6; i > 0; i--) {
+      if bh.substring(i-1, i) == '1'
+        $('.h' + i).classList.add('on')
+      
+    }
+  }, 10000)
+  
+})(window, document)
